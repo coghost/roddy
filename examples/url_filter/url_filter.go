90d4030a78b5766f7c5e754a4eedf96b6db28dc8
Hex <lihex.go@gmail.com>
2023-06-15 11:49:28 +0800

    feat: add example url_filter.

---
+++ examples/url_filter/url_filter.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"roddy"
+
+	"github.com/coghost/xlog"
+)
+
+func main() {
+	c := roddy.NewCollector(
+		roddy.URLFilters(
+			regexp.MustCompile("http://httpbin\\.org/(|e.+)$"),
+			regexp.MustCompile("https://github\\.com/req.+"),
+		),
+	)
+	defer c.HangUpInSeconds()
+	xlog.InitLogForConsole()
+
+	c.OnHTML("a[href]", func(e *roddy.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Printf("Link found: %q -> %s\n", e.Text(), link)
+
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnRequest(func(r *roddy.Request) {
+		fmt.Println("Visiting", r.String())
+	})
+
+	c.Visit("http://httpbin.org/")
+}
